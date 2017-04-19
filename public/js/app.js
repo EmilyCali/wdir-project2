5697aecf2032a3ca8e4189d1aac1a898cc6c7219
emilycali <cali_emily@bah.com>
2017-04-19 16:12:45 -0400

    Commit 33: discovered fadeTo and have it working, need to modify so that plants need water at different times

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,32 @@
-//get about the app link
+//get about the app link====================
 var $openModal= $("#openModal");
 //get modal
 var $modal = $("#modal");
 //get close link
 var $closeModal = $("#closeModal");
+//get images
+var $plantpics = $(".plantpic");
+//get watering feature
 
-//handlers
+//handlers ================================
+//when you click on the about this app link open the modal
 var openModal = function() {
   $modal.css("display", "block");
 };
-
+//when you click on the close link inside the modal close the modal
 var closeModal = function() {
   $modal.css("display", "none");
 };
+//function for fading out images
+$plantpics.fadeTo(2000, 0.4).delay(4000);
+//function for bringing back images when watering happens,
 
-//listeners
+
+
+//listeners================================
+//grabbing the open to click and run show
 $openModal.on("click", openModal);
+//grabbing the close to click and hide
 $closeModal.on("click", closeModal);
+
+//watering on click call function
