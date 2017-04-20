2b077e58d267e3fdb3f39242c12c338f8514615a
emilycali <cali_emily@bah.com>
2017-04-19 20:14:17 -0400

    Commit 36: trying to incorporate ajax, api.js created, usda plant index sourced

---
+++ public/js/api.js
@@ -0,0 +1,37 @@
+//this file is an attempt to attach an api to the show page for each plant
+
+//if else statements for images to say hey of the image is this then show the info on this page?
+$.ajax()
+
+
+
+//main page
+//https://plants.usda.gov/java/
+
+
+//poppies need twice
+//https://plants.usda.gov/core/profile?symbol=ESCA2#
+
+//tulips need three times
+//https://plants.usda.gov/core/profile?symbol=TULIP
+
+//lavender
+//https://plants.usda.gov/core/profile?symbol=LAAN81
+
+//pansy
+//https://plants.usda.gov/core/profile?symbol=VIBI
+
+//forget-me-not
+//https://plants.usda.gov/core/profile?symbol=MYSC
+
+//lily of the valley
+//https://plants.usda.gov/core/profile?symbol=COMA7
+
+//ranunculus
+//https://plants.usda.gov/core/profile?symbol=RASU4&photoID=rasu4_001_ahp.tif
+
+//morning glory
+//https://plants.usda.gov/core/profile?symbol=IPJA
+
+//blackeyed susan
+//https://plants.usda.gov/core/profile?symbol=RUHI2
